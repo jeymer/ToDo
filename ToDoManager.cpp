fbb5aa84cedf234a870544657ef54c1850c359f2
Jeffrey Eymer <jeymer@live.com>
2017-08-08 00:47:57 -0400

    Added functionality of opening saved ToDo list

--- ToDoManager.cpp
+++ ToDoManager.cpp
@@ -8,16 +8,30 @@ void invalidInputMessage() {
   std::cout << "Invalid input. Look at README.md for commands." << std::endl;
 }
 
-void openFile() {}
+std::string getFilePath() {
+  std::string file_path = getenv("HOME");
+  file_path.append("/ToDo.txt");
+  return file_path;
+}
 
 int main(int argc, char* argv[]) {
+  // Will hold tasks to be operated on
+  // Instantiated by opening saved file
   TaskList tasks;
-  std::fstream saved_file("~/ToDo.txt", std::fstream::in | std::fstream::out | std::fstream::trunc);
 
+  std::ifstream saved_file;
+  saved_file.open(getFilePath());
+
+  std::string line;
+  while(std::getline(saved_file, line)) {
+    std::string description = line.substr(0, line.find_first_of('~'));
+    std::string course = line.substr(line.find_first_of('~') + 1, (line.find_last_of('~') - line.find_first_of('~') - 1));
+    std::string due = line.substr(line.find_last_of('~') + 1, (line.length() - line.find_last_of('~') - 1));
+    tasks.addTask(description, course, due);
+  }
   
-  std::string homedir = getenv("HOME");
-
-  std::cout << homedir << std::endl;
+  
+  saved_file.close();
   
   /* ===== Check to ensure arguments ===== */
   if(argc == 1) {
@@ -27,6 +41,16 @@ int main(int argc, char* argv[]) {
   
   /* ===== Parse arguments and operate accordingly */
 
+  /* == List tasks in list == */
+  if(strcmp(argv[1], "list") == 0) {
+    /*= Should have no additional arguments to add =*/
+    if(argc != 2) {
+      invalidInputMessage();
+      exit(-1);
+    }
+    tasks.printTasks();
+  }
+  
   /* == Add new task == */
   if(strcmp(argv[1], "add") == 0) {
     /*= Should have no additional arguments to add =*/
